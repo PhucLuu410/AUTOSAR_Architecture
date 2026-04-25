3706c3ead690ae436780987a91a6106d05dc1715
PhucLuu410 <luuphuc20012001@gmail.com>
2026-04-25 14:21:32 +0700

    save current work

--- main.c
+++ main.c
@@ -8,14 +8,17 @@
 #include "ADC/Adc_Cfg.h"
 
 uint32_t Count = 0;
-uint16_t adcValueGr0[16] = {0};
+uint16_t *adcValue;
+uint16_t adcValue0[2];
+uint16_t adcValue1[2];
+Adc_StatusType status;
 void delay(volatile uint32_t t)
 {
     while (t--)
         ;
 }
 
-const Port_ConfigType Port_Configuration[NUMBER_OF_CHANNEL] = {
+const Port_ConfigType Port_Configuration[] = {
     [0] = {.port = PORT_A,
            .pin = 0,
            .mode = PORT_MODE_INPUT,
@@ -37,11 +40,8 @@ const Port_ConfigType Port_Configuration[NUMBER_OF_CHANNEL] = {
            .speed = PORT_OUTPUT_SPEED_50MHz,
            .cfg = PORT_CNF_ANALOG_INPUT}};
 
-Pwm_ConfigType Pwm_Configuration[4] = {
-    {0, 50, 0, 0, PWM_IDLE_STATE_HIGH, 1},
-    {1, 50, 60, 0, PWM_IDLE_STATE_LOW, 1},
-    {2, 50, 0, 0, PWM_IDLE_STATE_LOW, 1},
-    {3, 50, 40, 0, PWM_IDLE_STATE_LOW, 1}};
+Adc_ChannelType Adc_Group0_List[NUMBER_CHANNELS_OF_GROUP0] = {ADC_CHANNEL_0, ADC_CHANNEL_1};
+Adc_ReferenceType Adc_Group0_Reference[NUMBER_CHANNELS_OF_GROUP0] = {ADC_REFERENCE_0, ADC_REFERENCE_1};
 
 Adc_ChannelType Adc_Group1_List[NUMBER_CHANNELS_OF_GROUP0] = {ADC_CHANNEL_2, ADC_CHANNEL_3};
 Adc_ReferenceType Adc_Group1_Reference[NUMBER_CHANNELS_OF_GROUP0] = {ADC_REFERENCE_0, ADC_REFERENCE_1};
@@ -50,16 +50,29 @@ Adc_CommonConfigType Adc_Common_Configuration[] = {{ADC_1, ADC_CLOCK_DIV_2, ADC_
 
 Adc_ConfigType Adc_Configuration[NUMBER_OF_GROUPS] =
     {
-        {ADC_MODE_INDEPENDENT, ADC_GROUP_0, ADC_CONV_MODE_CONTINUOUS, ADC_CHANNEL_0, ADC_ALIGN_RIGHT, ADC_SAMPLING_TIME_239_5_CYCLES, ADC_RESOLUTION_12_BIT, ADC_REFERENCE_0, ADC_CLOCK_DIV_1},
-        {ADC_MODE_INDEPENDENT, ADC_GROUP_0, ADC_CONV_MODE_CONTINUOUS, ADC_CHANNEL_1, ADC_ALIGN_RIGHT, ADC_SAMPLING_TIME_239_5_CYCLES, ADC_RESOLUTION_12_BIT, ADC_REFERENCE_0, ADC_CLOCK_DIV_1},
-        {ADC_MODE_INDEPENDENT, ADC_GROUP_3, ADC_CONV_MODE_CONTINUOUS, ADC_CHANNEL_2, ADC_ALIGN_RIGHT, ADC_SAMPLING_TIME_239_5_CYCLES, ADC_RESOLUTION_12_BIT, ADC_REFERENCE_0, ADC_CLOCK_DIV_1},
-        {ADC_MODE_INDEPENDENT, ADC_GROUP_3, ADC_CONV_MODE_CONTINUOUS, ADC_CHANNEL_3, ADC_ALIGN_RIGHT, ADC_SAMPLING_TIME_239_5_CYCLES, ADC_RESOLUTION_12_BIT, ADC_REFERENCE_0, ADC_CLOCK_DIV_1}};
+        {Adc_Common_Configuration,
+         ADC_GROUP_0,
+         ADC_MODE_INDEPENDENT,
+         ADC_CONV_MODE_CONTINUOUS,
+         Adc_Group0_List,
+         Adc_Group0_Reference,
+         ADC_SAMPLING_TIME_239_5_CYCLES,
+         ADC_SWTRIGGER_SWS},
+
+        {Adc_Common_Configuration,
+         ADC_GROUP_1,
+         ADC_MODE_INDEPENDENT,
+         ADC_CONV_MODE_CONTINUOUS,
+         Adc_Group1_List,
+         Adc_Group1_Reference,
+         ADC_SAMPLING_TIME_239_5_CYCLES,
+         ADC_SWTRIGGER_SWS}};
 
 int main(void)
 {
 
-    RCC->CR |= (1 << 0);      // Enable HSI
-    RCC->APB2ENR |= (1 << 2); // Enable AFIO clock
+    RCC->CR |= (1 << 0);
+    RCC->APB2ENR |= (1 << 2);
     RCC->APB2ENR |= (1 << 4);
     RCC->APB2ENR |= (1 << 0);
     RCC->APB2ENR |= (1 << 9);
@@ -69,8 +82,9 @@ int main(void)
     Port_Init(Port_Configuration);
     Adc_Init(Adc_Configuration);
     status = Adc_GetGroupStatus(ADC_GROUP_1);
-    Adc_SetupResultBuffer(ADC_GROUP_0, adcValueGr0);
 
+    Adc_SetupResultBuffer(ADC_GROUP_1, adcValue0);
+    Adc_StartGroupConversion(ADC_GROUP_1);
     while (1)
     {
     }
