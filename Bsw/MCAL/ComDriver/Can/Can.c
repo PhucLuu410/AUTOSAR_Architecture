35e8e8011ead9b856c0887ed468d744b7f9aca10
PhucLuu410 <luuphuc20012001@gmail.com>
2026-05-28 18:56:28 +0700

    refactor Can Driver

--- Bsw/MCAL/ComDriver/Can/Can.c
+++ Bsw/MCAL/ComDriver/Can/Can.c
@@ -3,6 +3,15 @@
 #include "stm32f103xb.h"
 #include "CanIf.h"
 
+// Function Descriptions
+// The Can module writes the L-PDU in an appropriate buffer inside the CAN controller hardware
+// On L-PDU reception, the Can module calls the RX indication callback function with ID,Data Length and pointer to L-SDU as parameter.
+// The Can module provides an interface that serves as periodical processing function,and
+// which must be called by the Basic Software Scheduler module periodically
+// The Can module provides services to control the state of the CAN controllers.
+// Bus-off and Wake-up events are notified by means of callback functions.
+// The Can module is a Basic Software Module that accesses hardware resources.
+
 void Can_Init(const Can_ConfigType *Config)
 {
 }
