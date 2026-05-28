90ab1f8ffc6adaedc622b5cc7d432f0b0c3c51d1
PhucLuu410 <luuphuc20012001@gmail.com>
2026-05-28 17:19:14 +0700

    Continue Refactor Can Driver

--- Bsw/MCAL/ComDriver/Can/Can.h
+++ Bsw/MCAL/ComDriver/Can/Can.h
@@ -2,14 +2,62 @@
 #define CAN_H
 
 #include "ComStack_Types.h"
-#include "EcuM.h"
-// #include "Icu.h"
-#include "Os.h"
+#include "Icu.h"
 #include "Std_Types.h"
+#include "EcuM.h"
+#include "Os.h"
+
+// typedef struct
+// {
+
+// } Can_Filter;
+typedef struct
+{
+    uint32 Baudrate;
+    float32 Percent;
+} Can_Baudrate;
 
 typedef struct
 {
+    boolean CanSleepIsr;
+    boolean CanWakeupIsr;
+    boolean ErrorIsr;
+    boolean LastErrorCodeIsr;
+    boolean BusOffIsr;
+    boolean ErrorPassiveIsr;
+    boolean ErrorWarningIsr;
+    boolean Fifo1OverrunIsr;
+    boolean Fifo1OFullIsr;
+    boolean Fifo1MessagePendingIsr;
+    boolean Fifo0OverrunIsr;
+    boolean Fifo0OFullIsr;
+    boolean Fifo0MessagePendingIsr;
+    boolean TransmitMailboxEmptyIsr;
+} Can_Isr;
 
+typedef struct
+{
+    uint8 CanControllerNumber;
+    boolean CanDebugMode;
+    boolean CanTimerTriggerCommunicationMode;
+    boolean CanAutoBusOffMode;
+    boolean CanAutoWakeupMode;
+    boolean CanAutoRetransmission;
+    boolean CanReceiveFifoLockedMode;
+    boolean CanTransmitFifoPriority;
+} Can_Controller;
+
+typedef struct
+{
+} Can_HwUnit;
+
+typedef struct
+{
+    Can_Baudrate *CanBaudrate;
+    // Can_Filter *CanFilter;
+    Can_HwUnit *CanHwUnit;
+    Can_Controller *CanController;
+    Can_Isr *CanIsr;
 } Can_ConfigType;
 
 void Can_Init(const Can_ConfigType *Config);
--- Bsw/MCAL/ComDriver/Can/Can_Cfg.c
+++ Bsw/MCAL/ComDriver/Can/Can_Cfg.c
@@ -1,1 +1,61 @@
 #include "Can_Cfg.h"
+#include "Can.h"
+#include "Can_GeneralTypes.h"
+
+Can_Isr CanIsrUsed[] = {
+    {
+        .CanSleepIsr = FALSE,
+        .CanWakeupIsr = FALSE,
+        .ErrorIsr = FALSE,
+        .LastErrorCodeIsr = FALSE,
+        .BusOffIsr = FALSE,
+        .ErrorPassiveIsr = FALSE,
+        .ErrorWarningIsr = FALSE,
+        .Fifo1OverrunIsr = FALSE,
+        .Fifo1OFullIsr = FALSE,
+        .Fifo1MessagePendingIsr = FALSE,
+        .Fifo0OverrunIsr = FALSE,
+        .Fifo0OFullIsr = FALSE,
+        .Fifo0MessagePendingIsr = FALSE,
+        .TransmitMailboxEmptyIsr = FALSE,
+    },
+    {
+        .CanSleepIsr = TRUE,
+        .CanWakeupIsr = TRUE,
+        .ErrorIsr = TRUE,
+        .LastErrorCodeIsr = FALSE,
+        .BusOffIsr = FALSE,
+        .ErrorPassiveIsr = FALSE,
+        .ErrorWarningIsr = FALSE,
+        .Fifo1OverrunIsr = FALSE,
+        .Fifo1OFullIsr = FALSE,
+        .Fifo1MessagePendingIsr = FALSE,
+        .Fifo0OverrunIsr = FALSE,
+        .Fifo0OFullIsr = FALSE,
+        .Fifo0MessagePendingIsr = FALSE,
+        .TransmitMailboxEmptyIsr = TRUE,
+    }};
+
+Can_Controller CanController[NUMBER_OF_CAN_CONTROLLERS] = {
+    {
+        .CanControllerNumber = CAN_1_CONTROLLER,
+        .CanDebugMode = CAN_DEBUG_MODE_FREEZE,
+        .CanTimerTriggerCommunicationMode = CAN_TIMER_TRIGGER_COMMUNICATION_MODE_DISABLE,
+        .CanAutoBusOffMode = CAN_AUTO_BUS_OFF_MODE_ENABLE,
+        .CanAutoWakeupMode = CAN_AUTO_WAKEUP_MODE_DISABLE,
+        .CanAutoRetransmission = CAN_AUTO_RETRANSMISSION_ENABLE,
+        .CanReceiveFifoLockedMode = CAN_RECEIVE_FIFO_LOCKED_MODE_OVERWRITE,
+        .CanTransmitFifoPriority = CAN_TRANSMIT_FIFO_PRIORITY_BY_ID,
+    },
+};
+
+Can_HwUnit CanHwUnit[NUMBER_OF_CAN_HW_UNITS] = {
+    {
+
+    },
+};
+
+Can_ConfigType CanConfig = {
+    .CanHwUnit = &CanHwUnit[0],
+    .CanController = &CanController[0],
+    .CanIsr = &CanIsrUsed[0]};
